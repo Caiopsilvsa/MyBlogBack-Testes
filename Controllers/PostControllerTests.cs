b6468db5286c42947c7cb96b81d9de6d849a2ba0
caiopsilvsa <caio.psilva@outlook.com>
2022-12-13 11:35:05 -0300

    Teste do PostController

--- Controllers/PostControllerTests.cs
+++ Controllers/PostControllerTests.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using MyBlog.Controllers;
 using MyBlog.Dto;
+using MyBlog.Entities;
 using MyBlog.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -38,6 +39,42 @@ namespace MyBlog.Test.Controllers
             //Assert
             result.Should().NotBeNull();
             result.Should().BeOfType(typeof(Task<ActionResult<IEnumerable<PostDto>>>));
+        }
+
+        [Fact]
+        public void PostController_GetPostByName_ActionResult()
+        {
+            //Arrange
+            var titleName = "string";
+            var post = A.Fake<PostDto>();
+            A.CallTo(() => _postRepository.GetPostByName(titleName)).Returns(post);
+            var controller = new PostController(_postRepository, _mapper);
+
+            //Act
+            var result = controller.GetPostByName(titleName);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(Task<ActionResult<IEnumerable<PostDto>>>));
+
+        }
+
+        [Fact]
+        public void PostController_CreatePost_ActionResult()
+        {
+            //Arrange
+            var postUpdate = A.Fake<PostUpdateDto>();
+            var postMapped = A.Fake<Post>();
+            A.CallTo(() => _mapper.Map<Post>(postUpdate)).Returns(postMapped);
+            A.CallTo(() => _postRepository.NewPost(postMapped));
+            A.CallTo(() => _postRepository.SaveChanges()).Returns(true);
+            var controller = new PostController(_postRepository, _mapper);
+
+            //Act
+            var result = controller.CreatePost(postUpdate);
+
+            //Assert
+            result.Should().NotBeNull();
         }
     }
 }
